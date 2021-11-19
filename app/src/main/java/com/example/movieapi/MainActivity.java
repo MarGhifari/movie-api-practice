b7fbb1e4eeb7889b751b268a3b1e94322d8e9dde
MGC1 <marghifari8@gmail.com>
2021-11-19 20:41:26 +0700

    finishing

--- app/src/main/java/com/example/movieapi/MainActivity.java
+++ app/src/main/java/com/example/movieapi/MainActivity.java
@@ -5,14 +5,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.example.movieapi.Movies;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -42,9 +46,31 @@ public class MainActivity extends AppCompatActivity {
         JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
+                try {
+                    JSONArray items = response.getJSONArray("items");
+                    for (int i = 0; i < response.getInt("item_count"); i++) {
+                        JSONObject item = items.getJSONObject(i);
+                        String urlToImage = "https://image.tmdb.org/t/p/original/" + item.getString("poster_path");
+                        String title = item.getString("title");
+                        String description = item.getString("overview");
+                        String publishedAt = item.getString("release_date");
 
+                        Movies movies = new Movies(title, description, publishedAt, urlToImage);
+                        moviesList.add(movies);
+
+                    }
+                    MoviesAdapter adapter = new MoviesAdapter(MainActivity.this, moviesList);
+                    recyclerView.setAdapter(adapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-        })
-
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("TAG", error.toString());
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
